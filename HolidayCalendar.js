a1a686dfddd081cca531845a2322fbd2939a3017
beluyencode <longlc956@gmail.com>
2022-08-16 16:30:31 +0700

    1

--- HolidayCalendar.js
+++ HolidayCalendar.js
@@ -1,144 +1,144 @@
 module.exports = [
     {
-        startTime: '2022/04/30',
+        startTime: '4/30/2022',
         endTime: '01/05/2022',
         branch: ['115 Hoàng Quốc Việt, HN', 'Cơ sở 2 -  Tố Hữu'],
         note: 'Quốc lễ'
     },
     {
-        startTime: '2022/04/30',
+        startTime: '4/30/2022',
         endTime: '03/05/2022',
         branch: [],
         note: 'nghỉ'
     },
     {
-        startTime: '2022/04/30',
+        startTime: '4/30/2022',
         endTime: '01/05/2022',
         branch: [],
         note: 'Tết nguyên đán'
     },
     {
-        startTime: '2022/04/30',
+        startTime: '4/30/2022',
         endTime: '01/05/2022',
         branch: ['115 Hoàng Quốc Việt, HN'],
         note: 'Quốc lễ'
     },
     {
-        startTime: '2022/04/30',
+        startTime: '4/30/2022',
         endTime: '01/05/2022',
         branch: [],
         note: 'nghỉ'
     },
     {
-        startTime: '2022/04/30',
+        startTime: '4/30/2022',
         endTime: '03/05/2022',
         branch: ['Cơ sở 2 -  Tố Hữu'],
         note: 'Tết nguyên đán'
     },
     {
-        startTime: '2022/04/30',
+        startTime: '4/30/2022',
         endTime: '01/05/2022',
         branch: ['115 Hoàng Quốc Việt, HN', 'Cơ sở 2 -  Tố Hữu'],
         note: 'Quốc lễ'
     },
     {
-        startTime: '2022/04/30',
+        startTime: '4/30/2022',
         endTime: '01/05/2022',
         branch: [],
         note: 'nghỉ'
     },
     {
-        startTime: '2022/04/30',
+        startTime: '4/30/2022',
         endTime: '01/05/2022',
         branch: [],
         note: 'Tết nguyên đán'
     },
     {
-        startTime: '2022/04/30',
+        startTime: '4/30/2022',
         endTime: '03/05/2022',
         branch: ['115 Hoàng Quốc Việt, HN'],
         note: 'Quốc lễ'
     },
     {
-        startTime: '2022/04/30',
+        startTime: '4/30/2022',
         endTime: '01/05/2022',
         branch: [],
         note: 'nghỉ'
     },
     {
-        startTime: '2022/04/30',
+        startTime: '4/30/2022',
         endTime: '01/05/2022',
         branch: ['Cơ sở 2 -  Tố Hữu'],
         note: 'Tết nguyên đán'
     },
     {
-        startTime: '2022/04/30',
+        startTime: '4/30/2022',
         endTime: '01/05/2022',
         branch: ['115 Hoàng Quốc Việt, HN', 'Cơ sở 2 -  Tố Hữu'],
         note: 'Quốc lễ'
     },
     {
-        startTime: '2022/04/30',
+        startTime: '4/30/2022',
         endTime: '01/05/2022',
         branch: [],
         note: 'nghỉ'
     },
     {
-        startTime: '2022/04/30',
+        startTime: '4/30/2022',
         endTime: '01/05/2022',
         branch: [],
         note: 'Tết nguyên đán'
     },
     {
-        startTime: '2022/04/30',
+        startTime: '4/30/2022',
         endTime: '01/05/2022',
         branch: ['115 Hoàng Quốc Việt, HN'],
         note: 'Quốc lễ'
     },
     {
-        startTime: '2022/04/30',
+        startTime: '4/30/2022',
         endTime: '01/05/2022',
         branch: [],
         note: 'nghỉ'
     },
     {
-        startTime: '2022/04/30',
+        startTime: '4/30/2022',
         endTime: '01/05/2022',
         branch: ['Cơ sở 2 -  Tố Hữu'],
         note: 'Tết nguyên đán'
     },
     {
-        startTime: '2022/04/30',
+        startTime: '4/30/2022',
         endTime: '01/05/2022',
         branch: ['115 Hoàng Quốc Việt, HN', 'Cơ sở 2 -  Tố Hữu'],
         note: 'Quốc lễ'
     },
     {
-        startTime: '2022/04/30',
+        startTime: '4/30/2022',
         endTime: '01/05/2022',
         branch: [],
         note: 'nghỉ'
     },
     {
-        startTime: '2022/04/30',
+        startTime: '4/30/2022',
         endTime: '01/05/2022',
         branch: [],
         note: 'Tết nguyên đán'
     },
     {
-        startTime: '2022/04/30',
+        startTime: '4/30/2022',
         endTime: '01/05/2022',
         branch: ['115 Hoàng Quốc Việt, HN'],
         note: 'Quốc lễ'
     },
     {
-        startTime: '2022/04/30',
+        startTime: '4/30/2022',
         endTime: '01/05/2022',
         branch: [],
         note: 'nghỉ'
     },
     {
-        startTime: '2022/04/30',
+        startTime: '4/30/2022',
         endTime: '01/05/2022',
         branch: ['Cơ sở 2 -  Tố Hữu'],
         note: 'Tết nguyên đán'
